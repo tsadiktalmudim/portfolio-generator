94dcda0182ec60fc353acc7a32b919b5fabb07f0
Xan <tsadiktalmudim@gmail.com>
2022-03-29 11:31:35 -0500

    lesson 1

---
+++ app.js
@@ -0,0 +1,8 @@
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem =>
+        console.log(profileItem))
+};
+
+printProfileData(profileDataArgs);
