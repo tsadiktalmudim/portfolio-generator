f78b52562bbf5c3daa0646967deb79bf4f0caa85
Xan <tsadiktalmudim@gmail.com>
2022-04-03 07:30:27 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const fs = require('fs');
-const generatePage = require('.\\src\\page-template.js');
-var profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('.\\src\\page-template.js');
 
-const [userName, github] = profileDataArgs;
+// const pageHTML = generatePage(username, gitHub)
 
 
-fs.writeFile('index.html', generatePage(userName, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
